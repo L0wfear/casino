d915ebf2f6723ba6299258eb3663d1a6cfcfbd71
Your Name <you@example.com>
2020-05-11 00:01:30 +0300

    Updates

---
+++ precache-manifest.fb9d4b88e5597cc7c17c00a1b9ad968c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6766a1c5773b6be50254638b271d228",
+    "url": "/casino/index.html"
+  },
+  {
+    "revision": "621582f2bfc8f139cee4",
+    "url": "/casino/static/css/main.89f14245.chunk.css"
+  },
+  {
+    "revision": "096776f7b95230d3c159",
+    "url": "/casino/static/js/2.86efcbbc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/casino/static/js/2.86efcbbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "621582f2bfc8f139cee4",
+    "url": "/casino/static/js/main.117e57f1.chunk.js"
+  },
+  {
+    "revision": "7c6b74fefb8a0e656d3c",
+    "url": "/casino/static/js/runtime-main.d6009ecd.js"
+  }
+]);
