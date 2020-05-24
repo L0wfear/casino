ddd367917bee4e93a99dc47cf70ace4228afd0b7
Your Name <you@example.com>
2020-05-24 22:04:34 +0300

    loadbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Header from "./components/Header/Header";
+import BodyWrapper from "./components/CommonWrapper/BodyWraper";
+import Game from "./components/Game/Game";
 
 const App = () => {
   return (
     <div className="app_container">
       <Header />
       <Navbar />
-      <div className="main">
-      </div>
+      <BodyWrapper>
+        <Game />
+      </BodyWrapper>
     </div>
   );
 };
