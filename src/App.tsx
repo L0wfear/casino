d0d2ca55dec35224910f89b5c579a610bda5a936
Rafael <rafamg05@gmail.com>
2020-07-30 13:15:42 +0300

    some commits

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import React from "react";
-import Navbar from "./components/Navbar/Navbar";
-import Header from "./components/Header/Header";
-import BodyWrapper from "./components/CommonWrapper/BodyWraper";
-import Game from "./components/Game/Game";
+import React from 'react';
+import Navbar from './components/Navbar/Navbar';
+import Header from './components/Header/Header';
+import BodyWrapper from './components/CommonWrapper/BodyWraper';
+import Game from './components/Game/Game';
 
 const App = () => {
   return (
-    <div className="app_container">
+    <div className='app_container'>
       <Header />
       <Navbar />
       <BodyWrapper>
--- src/components/Common/Loadbar/Loadbar.tsx
+++ src/components/Common/Loadbar/Loadbar.tsx
@@ -1,22 +1,26 @@
-import React, { useState } from "react";
-import styled, { keyframes } from "styled-components";
-import zebra from "../../../img/zebra.png";
+import React, { useState } from 'react'
+import styled, { keyframes } from 'styled-components'
+import zebra from '../../../img/zebra.png'
 
 type LoadBarProps = {
-  width: number;
+  width: number
 }
 
 const SLoadBar = styled.div`
-    background: linear-gradient(to bottom, rgba(34, 49, 56, 0.5) 0, rgba(34, 49, 56, 0.5) 100%);
-    width: 400px;
-    height: 50px;
-    top: 50%;
-    right: 50%;
-    transform: translate(20%, -50%);
-    position: absolute;
-    overflow: hidden;
-    border-radius: 2px;
-`;
+  background: linear-gradient(
+    to bottom,
+    rgba(34, 49, 56, 0.5) 0,
+    rgba(34, 49, 56, 0.5) 100%
+  );
+  width: 400px;
+  height: 50px;
+  top: 50%;
+  right: 50%;
+  transform: translate(20%, -50%);
+  position: absolute;
+  overflow: hidden;
+  border-radius: 2px;
+`
 
 const translate = keyframes`
   from {
@@ -26,52 +30,50 @@ const translate = keyframes`
   to {
     transform: translateX(-30%);
   }
-`;
+`
 const SLoadBarImgWrap = styled.div<LoadBarProps>`
-    height: 100%;
-    width: ${props => props.width}%;
-    overflow: hidden;
-`;
+  height: 100%;
+  width: ${(props) => props.width}%;
+  overflow: hidden;
+`
 const SLoadBarImg = styled.div`
-    width: 1000px;
-    height: 100%;
-    background: url(${zebra}) #d0e805;
-    animation: ${translate} 8s linear infinite;
-`;
+  width: 1000px;
+  height: 100%;
+  background: url(${zebra}) #d0e805;
+  animation: ${translate} 8s linear infinite;
+`
 
 const SLoadBarText = styled.div`
-    position: absolute;
-    top: 50%;
-    right: 50%;
-    transform: translate(50%, -50%);
-    z-index: 1;
-    color: black;
-    font-size: 30px;
+  position: absolute;
+  top: 50%;
+  right: 50%;
+  transform: translate(50%, -50%);
+  z-index: 1;
+  color: black;
+  font-size: 30px;
 `
 
 const LoadBar = () => {
-
-  const [loadBarWidth, setLoadBarWidth] = useState(20);
+  const [loadBarWidth, setLoadBarWidth] = useState(20)
 
   const loadBarOnClick = () => {
     if (loadBarWidth >= 100) {
       setLoadBarWidth(0)
-    }
-    else {
-      setLoadBarWidth(loadBarWidth + 2);
+    } else {
+      setLoadBarWidth(loadBarWidth + 2)
     }
   }
 
   return (
     <SLoadBar onClick={loadBarOnClick}>
-        <SLoadBarImgWrap width={loadBarWidth}>
-           <SLoadBarImg />
-           <SLoadBarText>
-                <span> {loadBarWidth / 2}/50 </span>
-           </SLoadBarText>
-        </SLoadBarImgWrap>
+      <SLoadBarImgWrap width={loadBarWidth}>
+        <SLoadBarImg />
+        <SLoadBarText>
+          <span> {loadBarWidth / 2}/50 </span>
+        </SLoadBarText>
+      </SLoadBarImgWrap>
     </SLoadBar>
-  );
-};
+  )
+}
 
-export default LoadBar;
+export default LoadBar
